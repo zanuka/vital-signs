94d91f51a07cbc5f464843f87a14c29286e02060
zanuka <git@zanuka.com>
2021-05-13 23:25:57 +1200

    setup

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import { useHead } from '@vueuse/head'
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'vitalsigns',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'a simple control panel dashboard', content: 'prototype' },
   ],
 })
 </script>
